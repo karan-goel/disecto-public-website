dd42e6b0f19af2397878cc27209f2e0eb08af012
“Karan <kgoel95@gmail.com>
2021-04-20 22:43:12 +0530

    Made further modifications

---
+++ assets/js/fade.js
@@ -0,0 +1,16 @@
+var text = ["Securing", "Analysing"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+setInterval(change, 2500);
+
+function change() {
+    elem.classList.add('hide');
+    setTimeout(function () {
+        elem.innerHTML = text[counter];
+        elem.classList.remove('hide');
+        counter++;
+        if (counter >= text.length) {
+            counter = 0;
+        }
+    }, 500);
+}
